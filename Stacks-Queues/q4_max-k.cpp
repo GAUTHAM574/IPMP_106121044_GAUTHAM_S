b9c65d1e3f6047ad56b598640982b4ccedb1d38e
GAUTHAM S <98257025+GAUTHAM574@users.noreply.github.com>
2023-04-21 13:38:28 +0530

    Create q4_max-k.cpp

---
+++ Stacks-Queues/q4_max-k.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int>res;
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        deque<pair<int,int>> q;
+        for(int i =0 ; i < k; i++){
+            while(!q.empty() && q.back().first <= arr[i] )
+                q.pop_back();
+            q.push_back({arr[i], i});
+        }
+        res.push_back(q.front().first);
+        for(int i = k; i < n; i++){
+            while(!q.empty() && q.front().second <= i-k )
+                q.pop_front();
+            while(!q.empty() && q.back().first <= arr[i] )
+                q.pop_back();
+            q.push_back({arr[i], i});
+            res.push_back(q.front().first);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
