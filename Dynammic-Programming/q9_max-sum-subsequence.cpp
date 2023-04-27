8993c613f6663fc24e084b8f8cf56d51b5f44225
GAUTHAM S <98257025+GAUTHAM574@users.noreply.github.com>
2023-04-27 14:37:45 +0530

    Create q9_max-sum-subsequence.cpp

---
+++ Dynammic-Programming/q9_max-sum-subsequence.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    int dp[n] ;
+	    int res = 0;
+	    for(int i = 0; i < n; i++){
+	        dp[i] = arr[i];
+	        for(int j = 0 ; j < i ; j++){
+	            if(arr[i] > arr[j] ){
+	                int sum = dp[j] + arr[i];
+	                if(sum > dp[i]){dp[i] = sum;}
+	            }
+	        }
+	        res = max( res, dp[i]);
+	    }
+	    return res;
+	}  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
